1503b0dec2b591e9d0d2977816bed1d62f3f22c9
Juan Miguel Fontana <jmiguelfontana@gmail.com>
2026-08-09 15:56:03 +0200

    feat: add root API status route to web routes configuration

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,6 +11,3 @@ Route::get('/', function () {
     ]);
 });
 
-// Sobrescribir la ruta interna de Swagger para forzar que el archivo JSON vaya en la URL (/docs/api-docs.json)
-// y no como un parámetro de búsqueda (?api-docs.json), lo que puede ser bloqueado por Cloudflare WAF.
-Route::get('docs/api-docs.json', '\L5Swagger\Http\Controllers\SwaggerController@docs')->name('l5-swagger.default.docs');
